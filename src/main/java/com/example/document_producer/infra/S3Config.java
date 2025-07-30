824f5158f4c97b00613657c4594633b658bb988c
GBvaillant <vaillantgabriel@gmail.com>
2025-07-30 15:12:21 -0300

    feat: modify class s3 configuration

--- src/main/java/com/example/document_producer/infra/S3Config.java
+++ src/main/java/com/example/document_producer/infra/S3Config.java
@@ -4,33 +4,31 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
 
 import java.net.URI;
 
 @Configuration
 public class S3Config {
 
-    @Value("${aws.s3.endpoint}")
-    private String endpoint;
-
-    @Value("${aws.s3.region}")
-    private String region;
-
-    @Value("${aws.s3.accessKeyId}")
-    private String accessKeyId;
-
-    @Value("${aws.s3.secretAccessKey}")
-    private String secretAccessKey;
-
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
-                .endpointOverride(URI.create(endpoint))
-                .region(Region.of(region))
-                .credentialsProvider(() -> AwsBasicCredentials.create(accessKeyId, secretAccessKey))
+                .endpointOverride(URI.create("http://localhost:4566"))
+                .region(Region.of("us-east-1"))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create("test", "test")
+                        )
+                )
+                .serviceConfiguration(
+                        S3Configuration.builder()
+                                .pathStyleAccessEnabled(true)
+                                .build()
+                )
                 .build();
-
     }
 }
