dc63530c0e92336f6ad42d47f4addda1856cf833
GBvaillant <vaillantgabriel@gmail.com>
2025-07-31 11:34:06 -0300

    feat: add dto for request in controller

---
+++ src/main/java/com/example/document_producer/entity/SearchRequest.java
@@ -0,0 +1,13 @@
+package com.example.document_producer.entity;
+
+public class SearchRequest {
+    private String keyword;
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+}
